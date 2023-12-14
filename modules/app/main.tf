24f9a2528d4d333a6844b11fa581ac5b3330b102
Pushpak Vattam <vattampushpak243@gmail.com>
2023-12-14 08:02:41 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -53,4 +53,50 @@ resource "aws_autoscaling_group" "main" {
   }
 }
 
+resource "aws_iam_role" "main" {
+  name =  "${var.env}-${var.component}"
+  tags = merge(var.tags, {Name =  "${var.env}-${var.component}"})
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+}
+
+inline_policy {
+  name = "ssm_read_access"
+
+  policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+    {
+      "Sid": "GetResources",
+      "Effect": "Allow",
+      "Action": [
+      "ssm:GetParameterHistory",
+      "ssm:GetParametersByPath",
+      "ssm:GetParameters",
+      "ssm:GetParameter"
+      ],
+      "Resource": "arn:aws:ssm:us-east-1:072976934238:parameter/${var.env}.${var.component}.*"
+    },
+    {
+      "Sid": "ListResources",
+      "Effect": "Allow",
+      "Action": "ssm:DescribeParameters",
+      "Resource": "*"
+    }
+    ]
+
+  })
+}
