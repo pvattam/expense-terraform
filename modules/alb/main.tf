21eb5b92f749487e3d0dd1c042823bf574bf1eb3
Pushpak Vattam <vattampushpak243@gmail.com>
2023-12-20 08:24:18 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	roles/prometheus/tasks/main.yml
    #	roles/prometheus/templates/prometheus.yml

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -22,15 +22,15 @@ resource "aws_security_group" "main" {
   tags = merge(var.tags, {Name =  "${var.env}-${var.type}-alb"})
 }
 
-resource "aws_security_group_rule" "https" {
-  count = var.enable_https ? 1 : 0
-  from_port         = 443
-  protocol          = "tcp"
-  security_group_id = aws_security_group.main.id
-  to_port           = 443
-  type              = "ingress"
-  cidr_blocks       = var.sg_cidrs
-}
+#resource "aws_security_group_rule" "https" {
+#  count = var.enable_https ? 1 : 0
+#  from_port         = 443
+#  protocol          = "tcp"
+#  security_group_id = aws_security_group.main.id
+#  to_port           = 443
+#  type              = "ingress"
+#  cidr_blocks       = var.sg_cidrs
+#}
 
 resource "aws_lb" "main" {
   name               = "${var.env}-${var.type}"
@@ -42,7 +42,7 @@ resource "aws_lb" "main" {
 }
 
 resource "aws_lb_listener" "main" {
-  count = var.enable_https ? 0 : 1
+#  count = var.enable_https ? 0 : 1
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
@@ -53,19 +53,19 @@ resource "aws_lb_listener" "main" {
   }
 }
 
-resource "aws_lb_listener" "https" {
-  count = var.enable_https ? 1 : 0
-  load_balancer_arn = aws_lb.main.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.certificate_arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = var.target_group_arn
-  }
-}
+#resource "aws_lb_listener" "https" {
+#  count = var.enable_https ? 1 : 0
+#  load_balancer_arn = aws_lb.main.arn
+#  port              = "443"
+#  protocol          = "HTTPS"
+#  ssl_policy        = "ELBSecurityPolicy-2016-08"
+#  certificate_arn   = var.certificate_arn
+#
+#  default_action {
+#    type             = "forward"
+#    target_group_arn = var.target_group_arn
+#  }
+#}
 
 #resource "aws_lb_listener" "http" {
 #  count = var.enable_https ? 1 : 0
